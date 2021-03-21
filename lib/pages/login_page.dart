47e6d84cfcc21ca5120df7b971343af95387be39
ijazsharif <ijazsharif34@gmail.com>
2021-03-22 01:44:57 +0500

    Day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: Center(
+        child: Text(
+          "Login Page",
+          style: TextStyle(
+            // fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.orange,
+          ),
+          textScaleFactor: 2.0,
+        ),
+      ),
+    );
   }
 }
