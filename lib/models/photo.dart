c579a082fab3b45a22e377a09223352437a56de2
ijazsharif <ijazsharif34@gmail.com>
2021-03-22 14:39:07 +0500

    Model class photo.dart Created

---
+++ lib/models/photo.dart
@@ -0,0 +1,17 @@
+class Photo {
+  final num albumId;
+  final num id;
+  final String title;
+  final String thumbnailUrl;
+
+  Photo({this.albumId, this.id, this.title, this.thumbnailUrl});
+}
+
+final products = [
+  Photo(
+    albumId: 1,
+    id: 1,
+    title: "accusamus beatae ad facilis cum similique qui sunt",
+    thumbnailUrl: "https://via.placeholder.com/150/92c952",
+  )
+];
